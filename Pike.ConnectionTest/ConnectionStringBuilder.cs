e54bb1d413d9c2b42d524dd07ea97490ca5aa7a0
answering007 <answering007@gmail.com>
2025-05-05 03:33:46 +0300

    Сделано консольное приложение для тестирования фабрик

---
+++ Pike.ConnectionTest/ConnectionStringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Pike.OneS.Data;
+using Pike.OneS.WebService;
+
+namespace Pike.ConnectionTest
+{
+    internal class ConnectionStringBuilder
+    {
+        public static OneSDbConnectionStringBuilder OneSDbConnectionStringBuilder =>
+            new OneSDbConnectionStringBuilder
+            {
+                ProgId = SettingsConnection.Default.ProgId,
+                Database = SettingsConnection.Default.Database,
+                Server = SettingsConnection.Default.Server,
+                User = SettingsConnection.Default.User
+            };
+
+        public static WebServiceConnectionStringBuilder WebServiceConnectionStringBuilder =>
+            new WebServiceConnectionStringBuilder
+            {
+                Address = SettingsWebService.Default.Address,
+                UriNamespace = SettingsWebService.Default.UriNamespace,
+                Database = SettingsWebService.Default.Database,
+                ServiceFileName = SettingsWebService.Default.ServiceFileName,
+                UserName = SettingsWebService.Default.UserName,
+                Password = SettingsWebService.Default.Password
+            };
+    }
+}
---
+++ Pike.ConnectionTest/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using Pike.OneS.Data;
+using Pike.OneS.WebService;
+
+namespace Pike.ConnectionTest
+{
+    internal class Program
+    {
+        public const string BasicQuery = @"ВЫБРАТЬ 1, ДАТАВРЕМЯ(2018, 03, 15, 1, 2, 3), ""Привет"", ИСТИНА";
+
+        private static readonly IDictionary<char, Tuple<DbConnectionStringBuilder, string>> Builders =
+            new Dictionary<char, Tuple<DbConnectionStringBuilder, string>>
+            {
+                {
+                    '0',
+                    new Tuple<DbConnectionStringBuilder, string>(ConnectionStringBuilder.OneSDbConnectionStringBuilder,
+                        typeof(OneSDbProviderFactory).FullName)
+                },
+                {
+                    '1',
+                    new Tuple<DbConnectionStringBuilder, string>(ConnectionStringBuilder.WebServiceConnectionStringBuilder,
+                        typeof(WebServiceDbProviderFactory).FullName)
+                }
+            };
+
+        const string Separator = "=======================";
+
+        static void Main()
+        {
+            Console.WriteLine("Test connection:");
+            TestConnections();
+            Console.WriteLine(Separator);
+            Console.WriteLine("Done!");
+        }
+
+        static void TestConnections()
+        {
+            while (true)
+            {
+                Console.WriteLine(Separator);
+                Console.WriteLine("Choose connection:");
+                foreach (var item in Builders)
+                    Console.WriteLine($"[{item.Key}]: for [{item.Value.Item1.GetType().Name}]");
+
+                var symbol = Console.ReadKey();
+                Console.WriteLine("");
+                if (!Builders.TryGetValue(symbol.KeyChar, out var tuple)) break;
+                
+                var builder = tuple.Item1;
+                var factoryName = tuple.Item2;
+                Console.WriteLine(Separator);
+                Console.WriteLine($"Selected ConnectionString: {builder}");
+
+                var sw = new Stopwatch();
+                sw.Start();
+                try
+                {
+                    Console.WriteLine("Creating DbProviderFactory...");
+                    var factory = DbProviderFactories.GetFactory(factoryName);
+                    Console.WriteLine($"Factory created = {sw.Elapsed}");
+
+                    Console.WriteLine($"Creating connection...");
+                    using (var dbConnection = factory.CreateConnection())
+                    {
+                        Console.WriteLine($"Connection created = {sw.Elapsed}");
+                        if (dbConnection == null) throw new NullReferenceException("Connection is null");
+
+                        dbConnection.ConnectionString = builder.ConnectionString;
+                        Console.WriteLine("Opening connection...");
+                        dbConnection.Open();
+                        Console.WriteLine($"Connection opened = {sw.Elapsed}");
+
+                        Console.WriteLine("Creating command...");
+                        using (var dbCommand = factory.CreateCommand())
+                        {
+                            Console.WriteLine($"Command created = {sw.Elapsed}");
+                            if (dbCommand == null) throw new NullReferenceException("Command is null");
+
+                            dbCommand.Connection = dbConnection;
+                            dbCommand.CommandText = BasicQuery;
+                            var numberOfRows = dbCommand.ExecuteNonQuery();
+
+                            Console.WriteLine($"Number of rows = {numberOfRows}");
+                            Console.WriteLine($"Elapsed = {sw.Elapsed}");
+                        }
+                        Console.WriteLine("Command disposed");
+                    }
+                    Console.WriteLine("Connection disposed");
+                    Console.WriteLine("CONNECTION SUCCEEDED!");
+                    Console.WriteLine($"Total time = {sw.Elapsed}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+        }
+    }
+}
